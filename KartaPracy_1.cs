e17b3237c6c652bb8e41fd6f81044c7f809202e4
24kp <115844791+kkkaaarrr@users.noreply.github.com>
2022-10-15 01:05:54 +0200

    Add files via upload

---
+++ KartaPracy_1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace KartaPracy_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            // Zad 1
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a * a + Math.Pow(b, 2));
+            */
+
+            //Zad 2
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine((a + b)*(a + b));
+            */
+
+            //Zad 3
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine((a - b) * (a - b) * (a - b));
+            */
+            /*
+            //Zad 4 
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a * b * c);
+
+            Console.ReadKey();
+            */
+
+            //Zad 5
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(2 * (a + b) / 5);
+            */
+
+            // Zad 6
+
+            /*
+            double brutto = int.Parse(Console.ReadLine());
+
+            Console.WriteLine( brutto / 1.23 );
+            */
+
+            // Zad 7
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a % b);
+            */
+            
+          
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}

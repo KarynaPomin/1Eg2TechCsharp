6683e9c083d605961b728a94668ad075ec60e7c0
24kp <115844791+RichyKap@users.noreply.github.com>
2022-10-25 22:45:11 +0200

    Update KartaPracy_3b.cs

--- KartaPracy_3b.cs
+++ KartaPracy_3b.cs
@@ -6,7 +6,7 @@ namespace KartaPracy_3b
     {
         static void Main(string[] args)
         {
-            /*
+            
             Console.WriteLine("Zadanie 1");
 
             for (int i = 1; i < 32; i++)
@@ -16,8 +16,8 @@ namespace KartaPracy_3b
             
 
             Console.WriteLine("Zadanie 2\nLiczba:");
-            int n = int.Parse(Console.ReadLine());
-            for (int i = 1; i < n*2; i++)
+            int n2 = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n2*2; i++)
             {
                 if(i % 2 == 1)
                 {
@@ -51,74 +51,74 @@ namespace KartaPracy_3b
             }
             
             Console.WriteLine("Zadanie 5\nPodaj ile chcesz podać liczb:");
-            int n = int.Parse(Console.ReadLine());
-            int suma = 0;
-            for (int i = 0; i < n; i++)
+            int n5 = int.Parse(Console.ReadLine());
+            int suma5 = 0;
+            for (int i = 0; i < n5; i++)
             {
                 Console.WriteLine("Podaj liczbę:");
-                int a = int.Parse(Console.ReadLine());
-                suma += a;
+                int a5 = int.Parse(Console.ReadLine());
+                suma5 += a5;
             }
-            Console.WriteLine(suma);
+            Console.WriteLine(suma5);
             
             Console.Write("Zadanie 6\nPodaj liczbę:");
-            int k = int.Parse(Console.ReadLine());
-            int suma = 0; 
-            for (int i = 0; i < k; i++)
+            int k6 = int.Parse(Console.ReadLine());
+            int suma6 = 0; 
+            for (int i = 0; i < k6; i++)
             {
                 if(i % 2 == 0)
                 {
                     Console.WriteLine(i);  
-                    suma += i;
+                    suma6 += i;
                 }
             }
-            Console.WriteLine(suma);   
+            Console.WriteLine(suma6);   
             
-             */
+             
 
-            //Console.Write("Zadanie 7\nPodaj dwucyfrową liczbę:");
-            //int m = int.Parse(Console.ReadLine());
-            //int suma = 0;
-            //for (int i = 10; m >= i && m < 100; i++)
-            //{
-            //    if (i % 2 == 1)
-            //    {
-            //        suma += i;
-            //    }
-            //}
-            //if (m < 100 && m >= 10)
-            //{
-            //    Console.WriteLine(suma);
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Podana liczba nie jest dwucyfrowa.");
-            ////}
+            Console.Write("Zadanie 7\nPodaj dwucyfrową liczbę:");
+            int m7 = int.Parse(Console.ReadLine());
+            int suma7 = 0;
+            for (int i = 10; m7 >= i && m7 < 100; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    suma7 += i;
+                }
+            }
+            if (m7 < 100 && m7 >= 10)
+            {
+                Console.WriteLine(suma7);
+            }
+            else
+            {
+                Console.WriteLine("Podana liczba nie jest dwucyfrowa.");
+            }
 
-            //Console.WriteLine("Zadanie 8\nPodaj kwotę: ");
-            //double W = int.Parse(Console.ReadLine());
-            //Console.WriteLine("Podaj okres inwestycji w latach:");
-            //double L = int.Parse(Console.ReadLine());
-            //double wart = W;
-            //double wartM = 0;
-            //for (int i = 0; i < L * 12; i++)
-            //{
-            //    wartM = wart * 0.06 * 1 / 12f;
-            //    wart += wartM;
-            //}
-            //Console.WriteLine("Wartość inwestycji wynosi: " + Math.Round(wart, 2) + " zł.");
+            Console.WriteLine("Zadanie 8\nPodaj kwotę: ");
+            double W = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj okres inwestycji w latach:");
+            double L = int.Parse(Console.ReadLine());
+            double wart = W;
+            double wartM = 0;
+            for (int i = 0; i < L * 12; i++)
+            {
+                wartM = wart * 0.06 * 1 / 12f;
+                wart += wartM;
+            }
+            Console.WriteLine("Wartość inwestycji wynosi: " + Math.Round(wart, 2) + " zł.");
 
-            //Console.WriteLine("Zadanie 9\nPodaj liczbę:");
-            //int n9 = int.Parse(Console.ReadLine());
-            //int k9 = 21;
-            //int suma9 = 0;
-            //for (int i = 0; i < n9; i++)
-            //{
-            //    Console.WriteLine(k9);
-            //    k9 += 100;
-            //    suma9 = k9;
-            //}
-            //Console.WriteLine(suma9);
+            Console.WriteLine("Zadanie 9\nPodaj liczbę:");
+            int n9 = int.Parse(Console.ReadLine());
+            int k9 = 21;
+            int suma9 = 0;
+            for (int i = 0; i < n9; i++)
+            {
+                Console.WriteLine(k9);
+                k9 += 100;
+                suma9 = k9;
+            }
+            Console.WriteLine(suma9);
 
             Console.WriteLine("Zadanie 10");
             for (int i = 1; i < 1000; i++)

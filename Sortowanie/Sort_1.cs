49fa23d1b54ae6042c78727197c9c53b68f30705
pk22 <115844791+RichyKap@users.noreply.github.com>
2023-04-26 09:43:45 +0200

    Update Sort_1.cs

--- Sortowanie/Sort_1.cs
+++ Sortowanie/Sort_1.cs
@@ -73,15 +73,88 @@ for (int i = 1; i < n; i++)
     T[j] = temp;
 }
 
-// Sortowania w czasie liniowym
 
-// 4. Przez zliczanie
+// Sortowania w czasie liniowym - 0(n)
 
-// 5. Kubełkowe
+ // 4. Przez zliczanie (counting sort)
+
+int[] L = new int[20];
+
+for (int i = 0; i < n; i++)
+    L[T[i]]++;
+
+foreach (var item in L)
+    Console.Write(item + " ");
+
+int j = 0;
+for (int i = 0; i < L.Length; i++)
+{
+    if (L[i] > 0)
+    {
+        for (int k = 0; k < L[i]; k++)
+        {
+            T[j] = i;
+            j++;
+        }
+    }
+}
+
+// 5. Kubełkowe ---
 
 // Dziel i zwyciężaj 
 
 // 6. Przez scalanie
+void scalaj(int lewy, int prawy) // 0 - 3
+{
+    int[] pom = new int[n]; // Tablica pomocnicza
+    for (int a = 0; a < n; a++) pom[a] = T[a];
+    int i, i_lewy, i_prawy;
+    int srodek = (lewy + prawy) / 2;
+    i = lewy; // indeks tabeli
+    i_lewy = lewy;
+    i_prawy = srodek;
+
+    while (i_lewy <= srodek && i_prawy <= prawy)
+    {
+        if (pom[i_lewy] < pom[i_prawy])
+        {
+            T[i] = pom[i_lewy];
+            i_lewy++;
+        }
+        else{
+            T[i] = pom[i_prawy];
+            i_prawy++;
+        }
+        i++;
+    }
+    if (i_lewy > srodek)
+    {
+        while (i_prawy < prawy)
+        {
+            T[i] = pom[i_prawy];
+            i_prawy++;
+            i++;
+        }
+    }
+    else
+    {
+        while (i_lewy <= srodek)
+        {
+            T[i] = pom[lewy];
+            i_lewy++;
+            i++;
+        }
+    }
+}
+/// Nie dokończone do góry i na dole!!
+void sortuj(int lewy,int prawy)
+{
+    int srodek = (lewy + prawy) / 2;
+    if (lewy < prawy)
+        sortuj(lewy, srodek);
+    if (srodek + 1 < prawy)
+        // ...
+}
 
 // 7. Quicksort Hoare
 
